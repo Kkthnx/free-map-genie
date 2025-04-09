cad87203e39b9680928a38c64196056d24250903
MrFusiion <fuse-x-fuzionzz@outlook.be>
2025-04-09 21:20:21 +0200

    cleanup webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
-import path from "path";
-import url from "url";
-import fs from "fs";
+import "dotenv/config";
 
-import dotenv from "dotenv";
-dotenv.config();
+import path from "node:path";
+import fs from "node:fs";
 
 import HtmlWebpackPlugin from "html-webpack-plugin";
 import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
@@ -12,21 +10,22 @@ import WebExtPlugin from "web-ext-plugin";
 import CopyPlugin from "copy-webpack-plugin";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import { SwcMinifyWebpackPlugin } from "swc-minify-webpack-plugin";
-import { VueLoaderPlugin } from "vue-loader";
+
+const packageJson = JSON.parse(fs.readFileSync("./package.json", "utf-8"));
 
 import webpack from "webpack";
-const { DefinePlugin, ProvidePlugin } = webpack;
-
-const packageJson = JSON.parse(
-    fs.readFileSync("./package.json", { encoding: "utf-8" }).toString()
-);
 
 /**
- * Get the environment variable from the webpack env
- * @param {Record<string, any>} env
- * @param {string} name
- * @param {string[]} matches
- * @returns The value of the environment variable
+ * @typedef {Record<string, any>} Env;
+ * @typedef {"dev" | "prod"} ShortMode;
+ * @typedef {"development" | "production"} Mode;
+ */
+
+/**
+ * @param {Env} env 
+ * @param {string} name 
+ * @param {string[]} matches 
+ * @returns {string}
  */
 function getEnvVar(env, name, matches) {
     const value = env[name];
@@ -44,28 +43,29 @@ function getEnvVar(env, name, matches) {
 }
 
 /**
- * Parse the mode from the webpack env
- * @param {string} mode The mode to parse
- * @returns The parsed mode
+ * @param {ShortMode | Mode | undefined} mode
+ * @returns {Mode}
  */
 function parseMode(mode) {
-    return (
-        { dev: "development", prod: "production" }[mode] ||
-        mode ||
-        "development"
-    );
+    if (mode === "dev" || mode === undefined) {
+        return "development";
+    } 
+
+    if (mode === "prod") {
+        return "production";
+    }
+
+    return mode;
 }
 
 /**
- * Create the path to the dist folder
- * @param {string} browser The browser to create the path for
- * @param {string} mode The mode to create the path for
- * @returns The path to the dist folder
+ * @param {string} browser 
+ * @param {Mode} mode 
+ * @returns {string}
  */
 function distPath(browser, mode) {
-    const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
     return path.resolve(
-        __dirname,
+        import.meta.dirname,
         "dist",
         `fmg-${browser}-v${packageJson.version}${
             mode === "development" ? "-dev" : ""
@@ -73,22 +73,35 @@ function distPath(browser, mode) {
     );
 }
 
-/** @returns {import("webpack").Configuration} */
+/**
+ * @param {string} browser 
+ * @param {string} mode 
+ * @returns {string}
+ */
+function distName(browser, mode) {
+    return `fmg-${browser}-v${packageJson.version}` +
+        (mode === "production"
+            ? browser === "chrome"
+                ? ".zip"
+                : ".xpi"
+            : "");
+}
+
+/**
+ * @param {Env} env
+ * @returns {webpack.Configuration}
+ */
 export default (env) => {
-    const hostname = process.env.HOSTNAME || "localhost";
-    const port = JSON.parse(process.env.PORT || 3000);
     const watch = env.WEBPACK_WATCH || false;
 
     const browser = getEnvVar(env, "browser", ["chrome", "firefox"]);
-    const mode = parseMode(getEnvVar(env, "mode", ["dev", "prod"]));
+    const mode = parseMode(getEnvVar(env, "mode", ["dev", "prod", "development", "production"]));
 
     const isDev = mode !== "production";
 
     const dist = distPath(browser, mode);
 
     const GLOBALS = {
-        __HOSTNAME__: JSON.stringify(hostname),
-        __PORT__: port,
         __WATCH__: watch,
         __BROWSER__: JSON.stringify(browser),
         __MODE__: JSON.stringify(mode),
@@ -102,11 +115,6 @@ export default (env) => {
         { from: "./src/icons", to: "icons" },
         { from: "./src/font", to: "font" }
     ];
-
-    // Add the blocklist for chrome
-    // if (browser === "chrome") {
-    //     files.push({ from: "./src/rules.json", to: "rules.json" });
-    // }
 
     // Configure the webpack
     return {
@@ -123,13 +131,15 @@ export default (env) => {
             path: dist
         },
         resolve: {
-            extensions: [".tsx", ".ts", ".jsx", ".js", ".vue", ".json"],
-            alias: {
-                vue$: isDev
-                    ? "vue/dist/vue.runtime.esm-browser.js"
-                    : "vue/dist/vue.runtime.esm-browser.prod.js",
-            },
-            plugins: [new TsconfigPathsPlugin()]
+            extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+            plugins: [
+                new TsconfigPathsPlugin({
+                    configFile: path.resolve("tsconfig.web.json"),
+                })
+            ],
+        },
+        resolveLoader: {
+            modules: ["node_modules", "webpack/loaders"],
         },
         module: {
             rules: [
@@ -137,19 +147,8 @@ export default (env) => {
                     test: /\.(ts|tsx)$/,
                     exclude: /node_modules/,
                     use: [
-                        "import-glob",
                         "gnirts-loader",
-                        {
-                            loader: "ifdef-loader",
-                            options: {
-                                DEBUG: isDev,
-                                BROWSER: browser,
-                                VERSION: packageJson.version,
-                                CHROME: browser === "chrome",
-                                FIREFOX: browser === "firefox"
-                            }
-                        },
-                        path.resolve("./webpack/loaders/inject-globals.js"),
+                        "inject-globals",
                         {
                             loader: "swc-loader",
                             options: {
@@ -168,26 +167,8 @@ export default (env) => {
                     ],
                 },
                 {
-                    test: /\.vue$/,
-                    loader: "vue-loader"
-                },
-                {
-                    test: /\.css$/,
+                    test: /\.(s[ac]ss|css)$/,
                     use: [
-                        // "style-loader",
-                        MiniCssExtractPlugin.loader,
-                        {
-                            loader: "css-loader",
-                            options: {
-                                url: false
-                            }
-                        }
-                    ]
-                },
-                {
-                    test: /\.s[ac]ss/,
-                    use: [
-                        // "style-loader",
                         MiniCssExtractPlugin.loader,
                         {
                             loader: "css-loader",
@@ -226,19 +207,16 @@ export default (env) => {
                 }),
 
             // Provide global modules
-            new ProvidePlugin({
-                logger: [path.resolve(import.meta.dirname, "src", "fmg", "logger.ts"), "default"],
+            new webpack.ProvidePlugin({
+                logger: [path.resolve(import.meta.dirname, "src/fmg/logger.ts"), "default"],
                 React: "react"
             }),
 
             // Provide the global variables
-            new DefinePlugin(GLOBALS),
+            new webpack.DefinePlugin(GLOBALS),
 
             // Static files to copy to the dist folder
             new CopyPlugin({ patterns: files }),
-
-            // Vue loader
-            new VueLoaderPlugin(),
 
             // Generate the manifest
             new WebpackExtensionManifestPlugin({
@@ -253,13 +231,7 @@ export default (env) => {
             new WebExtPlugin({
                 sourceDir: dist,
                 artifactsDir: "./dist",
-                outputFilename:
-                    `fmg-${browser}-v${packageJson.version}` +
-                    (mode === "production"
-                        ? browser === "chrome"
-                            ? ".zip"
-                            : ".xpi"
-                        : ""),
+                outputFilename: distName(browser, mode),
                 overwriteDest: true,
                 target: browser === "chrome" ? "chromium" : "firefox-desktop",
                 devtools: true,
@@ -284,5 +256,5 @@ export default (env) => {
             minimize: !isDev,
             minimizer: [new SwcMinifyWebpackPlugin()]
         }
-    };
+    }
 };
